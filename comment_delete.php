86b2b5df1e0269888142a0d69a5e76f14bc97781
YasmeenAli16 <yasmeenali1698@gmail.com>
2021-10-13 22:12:17 +0200

    handle post body

--- comment_delete.php
+++ comment_delete.php
@@ -4,24 +4,26 @@ if (!empty($_SESSION["user"])) {
     $user = $_SESSION["user"];
     
     $comment_id  = $_GET["comment_id"];
-  // $post_id  = $_POST["post_id"];
+     $post_id  = $_GET["id"];
 
     require_once("config.php");
     $con = mysqli_connect(HOST_NAME , DB_UN ,DB_PW,DB_NAME,DB_PORT);
-    
-    
-    if ($user["role"] !="admin"){
-         $role ="and created_by=" .$user["id"];
-       
-        
+
+   
+    $qry = "select id from posts";
+    $rslt = mysqli_query($con, $qry);
+    if ($row = mysqli_fetch_assoc($rslt)){
         $qry = "delete from comments where id=$comment_id $role";
 
         $rslt = mysqli_query($con, $qry);
     }
     
+  
+
+    
     
     mysqli_close($con);
-    header("location:home.php");
+    header("location:home.php?");
     
 } else {
     header("location:login.php?secure=page");
--- single_post.php
+++ single_post.php
@@ -50,7 +50,8 @@ $post = mysqli_fetch_assoc($rslt)
             
                   
 <h4 class="text mt-3"><a class="text-dark" href="blog-single.html"><?= $post['title']?></a></h4>
-          <p><?= $post['body']?></p>
+          <p class="text pt-4" style="font-size:20px;"><?=wordwrap($post['body'],35,"<br>\n")?></p>
+
          
 </div>
 <img class="img-fluid mb-4" style="height:450px; width:450px; " src="<?= $post['image']?>">
@@ -96,7 +97,7 @@ $user = $_SESSION['user'];
 
 $post_id = $_GET['id'];
 
-$qry = "SELECT c.id, c.comment, c.created_by, c.created_at ,u.name FROM comments c join users u on (u.id = c.created_by) join posts p on (p.id = c.post_id) where p.id = $post_id order by c.created_at desc";
+$qry = "SELECT c.id, c.comment, c.post_id, c.created_by, c.created_at ,u.name FROM comments c join users u on (u.id = c.created_by) join posts p on (p.id = c.post_id) where p.id = $post_id order by c.created_at desc";
 
 require_once("config.php");
 $con = mysqli_connect(HOST_NAME , DB_UN ,DB_PW,DB_NAME,DB_PORT);
@@ -108,7 +109,7 @@ while($comment = mysqli_fetch_assoc($rslt)){
 
 
 ?>
-  
+   <input type="hidden" name="post_id">
   <i class="name" style=""><?= $comment['name'] ?></i> <br>
   <i style="color:red; font-size:13px"><?= $comment['created_at'] ?></i><br>
           <p style="margin-top:20px; font-size:20px"><?= $comment['comment'] ?></p>
